b3df8a7e4903b06aacaa733e26ef7bdcc77e9bce
Tomáš Volšanský <volsansky.tomas@frostbit.cz>
2015-02-02 22:51:28 +0100

    Class init

---
+++ src/Twitch/Twitch.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Twitch;
+
+class Twitch
+{
+
+  const API_URL   = 'https://api.twitch.tv/kraken';
+  const ACCEPT    = 'application/vnd.twitchtv.v3+json';
+
+  /* Get channel informations */
+  public function getChannel($name)
+  {
+    
+  }
+
+}
